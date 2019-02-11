cecadea6a93440f5ef62a419211c10429b0f1b48
Marek Praski <marekpraski@wp.pl>
2019-02-11 21:43:36 +0100

    initial setup

---
+++ src/test/programTests/FizzBuzzTest.java
@@ -0,0 +1,28 @@
+package programTests;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import program.FizzBuzz;
+
+public class FizzBuzzTest {
+    private FizzBuzz fb;
+    @Before
+    public void setUp() throws Exception {
+        fb = new FizzBuzz();
+    }
+
+    @Test
+    public void returnFizzWhenNumberDividedByThree(){
+
+    }
+    @Test
+    public void returnBuzzWhenNumberDividedByFive(){
+
+    }
+    @Test
+    public void returnFizzBuzzWhenNumberDividedByFifteen(){
+
+    }
+
+}
