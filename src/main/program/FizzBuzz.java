801df8502278e9c2f9bf5eead57afc1f40879643
Marek Praski <marekpraski@wp.pl>
2019-02-12 19:54:13 +0100

    tests set-up

--- src/main/program/FizzBuzz.java
+++ src/main/program/FizzBuzz.java
@@ -1,6 +1,19 @@
 package program;
 
+import java.util.List;
+
 public class FizzBuzz {
 
 
+    public List<String> getFizzBuzzList() {
+        return null;
+    }
+
+    public boolean isDividedByFive(int number) {
+        return true;
+    }
+
+    public boolean isDividedByThree(int number) {
+        return true;
+    }
 }
--- src/test/programTests/FizzBuzzTest.java
+++ src/test/programTests/FizzBuzzTest.java
@@ -5,6 +5,10 @@ import org.junit.Before;
 import org.junit.Test;
 import program.FizzBuzz;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class FizzBuzzTest {
     private FizzBuzz fb;
     @Before
@@ -14,15 +18,34 @@ public class FizzBuzzTest {
 
     @Test
     public void returnFizzWhenNumberDividedByThree(){
-
+        int[] numbers = {3, 9, 15};
+        for (int number : numbers) {
+            Assert.assertTrue(fb.isDividedByThree(number));
+        }
     }
     @Test
     public void returnBuzzWhenNumberDividedByFive(){
+        int[] numbers = {5, 10, 15};
+        for (int number : numbers) {
+            Assert.assertTrue(fb.isDividedByFive(number));
+        }
 
     }
     @Test
-    public void returnFizzBuzzWhenNumberDividedByFifteen(){
-
+    public void returnsItselfWhenNotDividedByThreeOrFive(){
+        Random random = new Random(100);
+        List<String> fizzBuzzList = fb.getFizzBuzzList();
+        for (int i=0; i<50; i++) {
+            Integer k = random.nextInt()+1;
+            if (!(k % 3 ==0 || k % 5 ==0)) {
+                Assert.assertEquals(fizzBuzzList.get(k), k.toString());
+            }
+        }
+    }
+    @Test
+    public void listContains100Item() {
+        List<String> fizzBuzzList = fb.getFizzBuzzList();
+        Assert.assertEquals(fizzBuzzList.size(),100);
     }
 
 }
